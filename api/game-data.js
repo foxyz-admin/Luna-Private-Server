6f6045fba170f44f749cdf43cb9390d6dba4c1cd
foxyz-admin <gustavonascimento82829@gmail.com>
2026-06-06 16:33:04 -0300

    Update game data response structure

--- api/game-data.js
+++ api/game-data.js
@@ -1,9 +1,4 @@
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json({
-        temporada_alvo: "2018",
-        status_servidor: "Operacional",
-        bypasses: ["SkinInject", "NameColor", "RetroLobby"]
-    });
+    res.status(200).json({ sistema: "Luna System 2018", status: "Online" });
 };
-
